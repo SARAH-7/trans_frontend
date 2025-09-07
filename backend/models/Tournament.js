1db72a413c194780f0872ffd4a1276158a5ddbdb
Sarah Abdalla <sa.abdallah98@gmail.com>
2025-09-07 21:01:50 +0400

    fml

--- backend/models/Tournament.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const tournamentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  players: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
-});
-
-export default mongoose.model("Tournament", tournamentSchema);
